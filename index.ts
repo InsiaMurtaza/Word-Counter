707c6793e6c6c4960f311c613138f1e05203a44b
InsiaMurtaza <165552743+InsiaMurtaza@users.noreply.github.com>
2024-05-23 15:43:56 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,26 @@
+import inquirer from "inquirer";
+let condition = true;
+while(condition){
+const answers:{sentences:string}= await inquirer.prompt([{
+    name:"sentences",
+    message:"Enter Your Text here!",
+    type: "input"
+}])
+
+let words= answers.sentences.trim().split(" ");
+
+console.log(`You have entered ${words.length} words`);
+
+let answer1 = await inquirer.prompt([{
+    name:"askquestion",
+    message:"Do you want to enter more text?",
+    type:"list",
+    choices:["Yes","No"]
+
+}])
+if( answer1.askquestion== "No"){
+condition = false;
+}
+else {
+condition = true;
+}}
